ca44be86b9dba99046e986cd7c3a5a004ae655b9
Bieldy <gabrieltotovikwork@gmail.com!>
2024-03-15 16:11:24 -0300

    Primeiro envio

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SIS_LIBERACAO_PRO_TWO
+{
+    class Program
+    {
+            static void Main (string [] args)
+            {
+                Console.Clear();
+                Console.WriteLine("Bem Vindo");
+            }
+    }
+}
